9166e31b9ca02b6a6e7c332ea218fbece73940d3
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-05 05:26:30 +0000

    Ran eslint to clean up code

--- iss.js
+++ iss.js
@@ -121,13 +121,13 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 /**
  * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
  * Input:
- *   - A callback with an error or results. 
+ *   - A callback with an error or results.
  * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The fly-over times as an array (null if error):
  *     [ { risetime: <number>, duration: <number> }, ... ]
- */ 
- const nextISSTimesForMyLocation = function(callback) {
+ */
+const nextISSTimesForMyLocation = function(callback) {
   fetchMyIP((error, IP) => {
     if (error) {
       return callback(error, null);
