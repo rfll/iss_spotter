80610f239160f0eebb3278b43a70d10781da0d3d
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-05 06:57:07 +0000

    Using promises to refactor iss assignment

---
+++ iss_promised.js
@@ -0,0 +1,30 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const parsedIP = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${parsedIP}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then((data) => {
+    const { response } = JSON.parse(data);
+    return response;
+  });
+};
+
+module.exports = { 
+  nextISSTimesForMyLocation
+ };
